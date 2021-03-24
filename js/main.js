b6c8da4c4b6f41e2cdb0121cfdd67feba62b7e3a
franzcrs <franz.chuquirachi@pucp.pe>
2021-03-24 00:24:35 -0500

    Navigation effect (between sections) added

--- js/main.js
+++ js/main.js
@@ -1,16 +1,55 @@
-const navBar = document.querySelector('.navigation1');
-const navElements = document.querySelectorAll('.navElem');
+const navBar = document.querySelector('.navBar');
+const navElements = navBar.querySelectorAll('.navElem');
+const sectionStripe = document.querySelector('.sectionsGrid');
 
 window.onload = function() {
     console.dir(navElements);
 };
 
-function navElemMDownHandler(e) {
+function animate({ timing, draw, duration }) {
+    let start = performance.now();
+    requestAnimationFrame(function animate(time) {
+        // timeFraction goes from 0 to 1
+        let timeFraction = (time - start) / duration;
+        if (timeFraction > 1) timeFraction = 1;
+        // calculate the current animation state
+        let progress = timing(timeFraction);
+        draw(progress); // draw it
+        if (timeFraction < 1) {
+            requestAnimationFrame(animate);
+        }
+    });
+}
+
+function scrollToSection(sectionNum) {
+    let sectionWidth = sectionStripe.children[0].getBoundingClientRect().width;
+    if (window.safari !== undefined || navigator.vendor == 'Apple Computer, Inc.') {
+        let startScrollLeft = sectionStripe.scrollLeft;
+        animate({
+            duration: 600,
+            timing: function quad(timeFraction) {
+                return Math.pow(timeFraction, 2);
+            },
+            draw: function scrollElem(progress) {
+                sectionStripe.scrollLeft = startScrollLeft - ((startScrollLeft - sectionNum * sectionWidth) * progress);
+            }
+        });
+    } else {
+        sectionStripe.scrollTo({
+            top: 0,
+            left: sectionNum * sectionWidth,
+            behavior: 'smooth'
+        });
+    }
+}
+
+function navElemMDownHandler(e, sectionNum) {
     e.preventDefault();
     navBar.querySelector('.navElemSelected').classList.remove('navElemSelected');
     e.target.closest('.navElem').classList.add('navElemSelected');
+    scrollToSection(sectionNum);
 }
 
 for (let i = 0; i < navElements.length; i++) {
-    navElements[i].addEventListener('mousedown', navElemMDownHandler);
+    navElements[i].addEventListener('mousedown', (e) => { navElemMDownHandler(e, i) });
 }
